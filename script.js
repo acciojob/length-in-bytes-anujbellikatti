fb70faae3f3b3dcb2e94c3c2a0edc07b07f9795c
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-20 01:36:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const byteSize = (str) => {
   // write your code here
+	let x = str.split("");
+	if(x[0]>="a" && x[0]<="z"){
+		return x.length
+	}else{
+		return x.length*5;
+	}
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+// const str = prompt("Enter some string.");
+// alert(byteSize(str));
